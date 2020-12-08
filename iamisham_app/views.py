8915103612eaccdbfa46f93b46648fb5c49d2607
isibrahi <isibrahi@akamai.com>
2020-12-08 16:44:12 +0530

    Chaning template

---
+++ iamisham_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from iamisham_site.settings import STATIC_URL
+from django.template import loader
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    template = loader.get_template("iamisham_app/index.html")
+    context = {
+        "STATIC_URL": STATIC_URL
+    }
+
+    return HttpResponse(template.render(context, request))
