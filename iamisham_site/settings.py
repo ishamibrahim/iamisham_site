decb5df9eb5874e3831745e12928d69e21ee7aa6
Isham Ibrahim <isibrahi@blr-mp4mt.bangalore.corp.akamai.com>
2023-01-09 15:45:01 +0530

    Statis configuration

--- iamisham_site/settings.py
+++ iamisham_site/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_DIRS = [BASE_DIR / "iamisham_app/static"]
-# STATIC_ROOT = BASE_DIR / "iamisham_app/static"
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "iamisham_app"
+STATIC_URL = 'static/'
