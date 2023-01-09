b705b9e1c3c9b0d0265b0a5e89b0f14b7fc38a70
Isham Ibrahim <isibrahi@blr-mp4mt.bangalore.corp.akamai.com>
2023-01-09 23:31:26 +0530

    Static files resolving

--- iamisham_site/settings.py
+++ iamisham_site/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / "iamisham_app/static"]
-STATIC_ROOT = BASE_DIR / "iamisham_app"
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "iamisham_app/static")]
+STATIC_ROOT = "/var/www/ishamibrahim/static/"
+STATIC_URL = '/static/'
