caa865e4a2c0015fe8380f44e479a500d7557d80
isham <eyash.27@gmail.com>
2023-07-02 23:08:57 +0530

    index html change

--- iamisham_site/settings.py
+++ iamisham_site/settings.py
@@ -125,7 +125,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSSsudo tail -F /var/log/nginx/error.log , JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "iamisham_app/static")]
